50a070c317c0eab4f646aefb75f48e81334f0aa0
Sunny0714 <suhan@s.sfusd.edu>
2026-02-03 11:58:41 -0800

    M6L6

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -34,3 +34,16 @@ func open() -> void:
 	animation_player.play("Open")
 
 	input_pickable = false
+	for current_index in range(randi_range(1, 3)):
+		_spawn_random_item()
+	
+func _spawn_random_item() -> void:
+	var loot_item: Area2D = possible_items.pick_random().instantiate()
+	add_child(loot_item)
+	var random_angle := randf_range(0.0, 2.0 * PI)
+	var random_direction := Vector2(1.0, 0.0).rotated(random_angle)
+	var random_distance := randf_range(40.0, 100.0)
+	loot_item.position = random_direction * random_distance
+
+
+@export var possible_items: Array[PackedScene] = []
